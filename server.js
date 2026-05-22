9645abb4d1b15ebae09656a1ee64e3545ca4dd6a
stanislaukavalenka-coder <stanislaukavalenka@gmail.com>
2026-05-22 16:28:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const { google } = require('googleapis');
 const fs = require('fs');
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 const app = express();
 app.use(express.json());
 
